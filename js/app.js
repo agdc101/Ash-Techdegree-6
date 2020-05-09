194a05fa2887a2df51d17457bbb0c6a1eb779bce
Ashley Campbell <agdc101@gmail.com>
2020-05-10 00:59:42 +0100

    Day 2
    JS done. Almost

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 // ------ TITLES ARRAY -------- //
 const movieTitles = [
-    ['JURASSIC PARK'],
-    ['BACK TO THE FUTURE'],
-    ['SAVING PRIVATE RYAN'],
-    ['GUARDIANS OF THE GALAXY'],
-    ['FORREST GUMP'],
-    ['SLUMDOG MILLIONAIRE'],
-    ['THE BREAKFAST CLUB'],
-    ['GANGS OF NEW YORK'],
-    ['CASABLANCA'],
-    ['GLADIATOR']
+    ['Jurassic Park'],
+    ['Back To The Future'],
+    ['Saving Private Ryan'],
+    ['Guardians Of The Galaxy'],
+    ['Forrest Gump'],
+    ['Slumdog Millionaire'],
+    ['The Breakfast Club'],
+    ['Gangs Of New York'],
+    ['Casablanca'],
+    ['Gladiator']
 ];
 // ---------------------------- //
 // ------- VARIABLES ---------- //
-const letters = document.querySelector('#qwerty');
+const keyboard = document.querySelector('#qwerty');
 const movieTitle = document.querySelector('#movieTitle');
-let missed = 0;
+let missed = -1;
+const lives = document.querySelectorAll('.tries');
+const startButton = document.querySelector('.btn__reset');
+const overlay = document.querySelector('#overlay');
+const heading =  document.querySelector('h2');
 // ---------------------------- //
 // ------- FUNCTIONS ---------- //
 function getMovieAsArray (movie) {
@@ -30,36 +34,71 @@ function addMovieToDisplay (movie) {
     li.append(letter);    
     if ( letter !== ' ' ) {
         li.classList.add('letter');
+    } else {
+        li.classList.add('space');
     }
     movieTitle.append(li);
     }
 };
 
-const test = getMovieAsArray(movieTitles[5]);
-addMovieToDisplay(test);
+function createRandomTitle () {
+    return Math.round(Math.random() * 10);
+};
+
+
+addMovieToDisplay(getMovieAsArray(movieTitles[createRandomTitle()]));
 
 function checkLetter (buttonPress) {
     const letter  = document.querySelectorAll('.letter');
-    
-    for ( let j = 0; j < letter.length; j += 1 ) {
-        if ( letter[j].textContent === buttonPress.textContent  ) {
-            letter[j].classList.add('show');
-            let letterMatch = letter[j].textContent;
-            return letterMatch;
-        } else {
-            return null;
-        };
-
+    let letterMatch = null;
+    for ( let j = 0; j < letter.length; j += 1 ) {       
+        if ( letter[j].textContent.toLowerCase() === buttonPress.textContent  ) {
+            letter[j].classList.add('show');  
+            letterMatch = letter[j].textContent;                                  
+        }       
     };
+    return letterMatch;    
 };
-console.log(checkLetter());
+
+keyboard.addEventListener('click', (e) => { 
+    if ( e.target.tagName === 'BUTTON' ) { 
+    e.target.classList.add('chosen');
+    e.target.setAttribute('disabled', '');
+        if (checkLetter(e.target) === null) {
+            alert('wrong');
+            missed += 1;
+            lives[missed].style.display = 'none';
+        }   
+        
+    }
+    
+    setTimeout(function(){ checkWin(); }, 4000);
+});
+
+function checkWin () {
+    let titleLetters = document.querySelectorAll('.letter');
+    let correctGuesses = document.querySelectorAll('.show');
+    
+    if ( titleLetters.length === correctGuesses.length) {
+        overlay.classList.add('win');
+        overlay.style.display = 'flex';
+        heading.textContent = 'Well Done You Won!';
+
+    } else if ( missed === 4 ) {    
+        overlay.classList.add('lose');
+        overlay.style.display = 'flex';
+        heading.textContent = 'You Silly Bitch!';
+    }
+    
+};
+
+
 
 // ---------------------------- //
 // Hide overlay on button click //
-const startButton = document.querySelector('.btn__reset');
-const overlay = document.querySelector('#overlay');
+
 
 startButton.addEventListener('click', () => {
-    overlay.style.opacity = 0;
+    overlay.style.display = 'none';
 });
 // ---------------------------- //
