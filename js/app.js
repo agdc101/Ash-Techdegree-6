ba3679dfae4b319d80c302fa8e7c84dcdc5442a8
Ashley Campbell <agdc101@gmail.com>
2020-05-11 12:50:53 +0100

    Review
    ready for review

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// ------ TITLES ARRAY -------- //
+// -------- TITLES ARRAY ------- //
 const movieTitles = [
     ['Jurassic park'],
     ['Back to the future'],
@@ -14,28 +14,35 @@ const movieTitles = [
     ['The italian job'],
     ['Kill bill'],
     ['The blues brothers'],
-    ['Blade runner']
+    ['Blade runner'],
+    ['Mrs doubtfire'],
 ];
 // ---------------------------- //
 // ------- VARIABLES ---------- //
 const keyboard = document.querySelector('#qwerty');
 let movieTitle = document.querySelector('#movieTitle ul');
-let missed = -1;
 let lives = document.querySelectorAll('.tries');
 const startButton = document.querySelector('.btn_start');
 const resetButton = document.querySelector('.btn_reset');
 const overlay = document.querySelector('#overlay');
 const heading =  document.querySelector('h2');
+const message =  document.querySelector('p');
 let currentMovie = '';
+let missed = -1;
 
-// ---------------------------- //
-// ------- FUNCTIONS ---------- //
-function getMovieAsArray (movie) {
+// ------------------------------ //
+// --------- FUNCTIONS ---------- //
+
+/* -- Turns the selected movie into an array--  */
+const getMovieAsArray = movie => {
     const film = String(movie);
     return film.split('');
 };
+// ------------------------------ //
 
-function addMovieToDisplay (movie) {
+/* -- the movie array is then passed here, each letter is made into an
+<li> element then appended to the page --s*/
+const addMovieToDisplay = movie => {
     for (let j = 0; j < movie.length; j += 1) {
     const li = document.createElement('li');
     const letter = `${movie[j]}`;
@@ -48,14 +55,14 @@ function addMovieToDisplay (movie) {
     movieTitle.append(li);
     }
 };
+// -------------------------------- //
 
-function createRandomTitle () {
-    return Math.round(Math.random() * 14);
-};
+/* -- Math function creates number between 0 and 15 --*/
+const createRandomTitle = () =>  Math.round(Math.random() * 15);
 
-
-
-function checkLetter (buttonPress) {
+/*  --This function compares the text content of the button 'clicked', to each
+letter in the movie title --*/
+const checkLetter = (buttonPress) => {
     const letter  = document.querySelectorAll('.letter');
     let letterMatch = null;
     for ( let j = 0; j < letter.length; j += 1 ) {       
@@ -66,6 +73,12 @@ function checkLetter (buttonPress) {
     };
     return letterMatch;    
 };
+// ------------------------------------- //
+
+/*-- Game keyboard listener. Applies classes
+to buttons that have been 'chosen' and selections that are 'wrong' and disables
+the button pressed so it cannot be selected twice.
+If  wrong, a life is made opaque and 'missed' is increased by +1 --*/
 
 keyboard.addEventListener('click', (e) => { 
     if ( e.target.tagName === 'BUTTON' ) { 
@@ -77,50 +90,70 @@ keyboard.addEventListener('click', (e) => {
             lives[missed].style.opacity = 0.2;
         }           
     }
-    
-    setTimeout(function(){ checkWin(); }, 4500);
+    checkWin(); 
 });
+// ------------------------------------- //
 
-function resultPage (result) {
-    overlay.classList.add(result);
-    overlay.style.display = 'flex';
-    
+/*-- Function checking if the user has won or lost --*/
+const checkWin = () => {
+    let titleLetters = document.querySelectorAll('.letter');
+    let correctGuesses = document.querySelectorAll('.show');   
+    if ( titleLetters.length === correctGuesses.length) {
+        resultPage ('win','Well Done You Won!');
+        hideShowButtons ();
+        message.textContent = `${chosenMovie} was correct!`;
+    } else if ( missed >= 4 ) {    
+        resultPage ('lose','You Lost!');
+        hideShowButtons ();
+        message.textContent = `The answer was ${chosenMovie}!`;       
+    }    
+};
+// ------------------------------------- //
+
+/* --This function adds the correct class based on the result,
+adds the lose or win message then displays the overlay-- */ 
+const resultPage = (resultOverlay, message) => {
+    overlay.classList.add(resultOverlay);
+    heading.textContent = message;
+    overlay.style.display = 'flex';    
 }
 
-function checkWin () {
-    let titleLetters = document.querySelectorAll('.letter');
-    let correctGuesses = document.querySelectorAll('.show');
-    
-    
-    if ( titleLetters.length === correctGuesses.length) {
-        resultPage ('win');
-        hideShowButtons ();
-        heading.textContent = `Well Bloody Done! it was ${currentMovie}`;
-
-    } else if ( missed >= 4 ) {    
-        // console.log(currentMovie);
-        resultPage ('lose');
-        hideShowButtons ();
-        heading.textContent = `You Silly Billy! it was ${currentMovie}`;
-        
-    }
-    
-};
-function hideShowButtons () {
+/* --This function hides the 'start button and displays
+the 'reset' button on the overlays-- */
+const hideShowButtons =() => {
     startButton.style.display = 'none';
     resetButton.style.display = 'inline-block';
 }
 
+// ----------------------------------- //
 
-// ---------------------------- //
-// Hide overlay on button click //
-resetButton.addEventListener('click', () => {
-    
-    let letters  = document.querySelectorAll('#movieTitle ul li');
+// reset and start button listener and functions //
+resetButton.addEventListener('click', () => {   
+    pageReset(); 
+    runProgram();  
+})
+
+startButton.addEventListener('click', () => {  
+    runProgram();
+});
+// ----------------------------------- //
+
+/* --This functions runs the program! a random movie is selected and 
+passed through to be made into an array then displayed --*/
+const runProgram = () => {
+    chosenMovie = movieTitles[createRandomTitle()];   
+    addMovieToDisplay(getMovieAsArray(chosenMovie));
+    overlay.style.display = 'none';
+}
+// ----------------------------------- //
+
+/*-- this function resets the page. classes and the previous movie title are removed,
+ the users lives are restored and the missed variable is reset-- */
+const pageReset = () => {
+    let letters  = movieTitle.querySelectorAll('li');
     let keyboardButtons  = document.querySelectorAll('.keyrow button');
     for ( let j = 0; j < letters.length; j += 1 ) {       
-        letters[j].remove();
-    
+        letters[j].remove();    
     }
     for ( let j = 0; j < lives.length; j += 1 ) {
         lives[j].style.opacity = 1;
@@ -128,19 +161,8 @@ resetButton.addEventListener('click', () => {
     for ( let j = 0; j < keyboardButtons.length; j += 1 ) {
         keyboardButtons[j].className = "";
         keyboardButtons[j].removeAttribute('disabled');
-    }
-  
+    }  
     overlay.className = '';
     missed = -1;
-    overlay.style.display = 'none';
-    currentMovie = movieTitles[createRandomTitle()];
-    addMovieToDisplay(getMovieAsArray(currentMovie));
-})
-
-
-startButton.addEventListener('click', () => {  
-    currentMovie = movieTitles[createRandomTitle()];
-    overlay.style.display = 'none';   
-    addMovieToDisplay(getMovieAsArray(currentMovie));
-});
-// ---------------------------- //
+}
+// ----------------------------------- //
